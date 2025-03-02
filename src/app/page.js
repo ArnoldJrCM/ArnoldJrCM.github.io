3ec1eae4a662964f09f587225047afcd896ce1b4
malataarno <arnold.malatajr25@gmail.com>
2025-03-02 21:07:43 +1300

    Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="wrapper">
+      <header>
+        <h1>Header </h1>
+        
+      </header>
+      <main>
+        <div className="maincontent">
+          <h1> hello world </h1>
+        </div>
+      </main>
+      <footer>
+        Footer
+      </footer>
     </div>
   );
 }
